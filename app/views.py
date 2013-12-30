70b6036ebc556c6c9a5f1b67d05668222c46ee4a
designhawg <designhawg@mac.com>
2013-12-30 16:44:01 -0600

    pushing up the project at the end of lesson 3

---
+++ app/views.py
@@ -0,0 +1,44 @@
+from flask import render_template, flash, redirect
+from app import app
+from forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Miguel' }
+    posts = [
+      {
+        'author': { 'nickname': 'John' },
+        'body': 'Beautiful day in Portland!'
+      },
+      {
+        'author': { 'nickname': 'Chris' },
+        'body': 'Woo Pig Sooie!'
+      },
+      {
+        'author': { 'nickname': 'Riley' },
+        'body': 'I love Frozen'
+      },
+      {
+        'author': { 'nickname': 'Karen' },
+        'body': 'I don\'t wanna be here'
+      }
+    ]
+
+    return render_template("index.html",
+      title = "homer",
+      user = user,
+      posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+      flash('Login requested for OpenID = "' + form.openid.data + '", remember_me ' + str(form.remember_me.data))
+      return redirect('/index')
+
+    return render_template("login.html",
+      title = "Sign In",
+      form = form,
+      providers = app.config['OPENID_PROVIDERS'])
---
+++ config.py
@@ -0,0 +1,8 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'test'
+
+OPENID_PROVIDERS = [
+        {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+        {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+        {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+        {'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
