65aaf59083d87f404ad5b79d7847b6056fc545a4
designhawg <designhawg@mac.com>
2014-01-02 07:42:27 -0600

    added user profile info

--- app/views.py
+++ app/views.py
@@ -84,6 +84,26 @@ def after_login(resp):
 def load_user(id):
     return User.query.get(int(id))
 
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+
+    posts = [
+        { 'author': user, 'body': 'Test post #1' },
+        { 'author': user, 'body': 'Text post #2' }
+    ]
+
+    return render_template( 'user.html',
+        user = user,
+        posts = posts )
+
+
+
 @app.route('/logout')
 def logout():
     logout_user()
