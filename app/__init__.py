8435bdab5b5c51de241c7118c803420a12ddbd9f
designhawg <designhawg@mac.com>
2014-01-08 00:02:27 -0600

    fixed error I accidentally shoved into the repo by moving my __init__ file out of order

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
-from app import views, models
-
-
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -39,3 +42,4 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.info('microblog startup')
 
+from app import views, models
