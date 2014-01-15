154d4a1ee425a56bc86d31d2ee38ab24b87303ef
designhawg <designhawg@mac.com>
2014-01-15 00:38:10 -0600

    found an error in followed posts that was crushing the whole fucking app

--- app/models.py
+++ app/models.py
@@ -84,7 +84,7 @@ class User(db.Model):
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        return Post.query.join( followers, (followers.c.followed_id == Post.user_id)).filter(follower.s.c.follower_id == self.id).order_by(Post.timestamp.desc())
+        return Post.query.join( followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
 
 
 
--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm, EditForm
-from models import User, ROLE_USER, ROLE_ADMIN
+from forms import LoginForm, EditForm, PostForm
+from models import User, ROLE_USER, ROLE_ADMIN, Post
 from datetime import datetime
 
 @app.before_request
@@ -31,33 +31,23 @@ def internal_error(error):
     return render_template('500.html'), 500
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/index', methods = ['GET', 'POST'])
 @login_required
 def index():
-    user = g.user
-    posts = [
-      {
-        'author': { 'nickname': 'John' },
-        'body': 'Beautiful day in Portland!'
-      },
-      {
-        'author': { 'nickname': 'Chris' },
-        'body': 'Woo Pig Sooie!'
-      },
-      {
-        'author': { 'nickname': 'Riley' },
-        'body': 'I love Frozen'
-      },
-      {
-        'author': { 'nickname': 'Karen' },
-        'body': 'I don\'t wanna be here'
-      }
-    ]
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body = form.post.data, timestamp = datetime.utcnow(), author = g.user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+
+    posts = g.user.followed_posts().all()
 
     return render_template("index.html",
         title = "Home",
-        user = user,
+        form = form,
         posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
