fbb1bb5d569a62f82230c40717ed507d6e198610
designhawg <designhawg@mac.com>
2014-01-16 00:11:08 -0600

    End of lesson 9. Pagination included for user page

--- app/views.py
+++ app/views.py
@@ -119,18 +119,16 @@ def edit():
     return render_template('edit.html', form = form)
 
 @app.route('/user/<nickname>')
+@app.route('/user/<nickname>/<int:page>')
 @login_required
-def user(nickname):
+def user(nickname, page = 1):
     user = User.query.filter_by(nickname = nickname).first()
 
     if user == None:
-        flash('Hey, the person named ' + nickname + ' could not found.')
+        flash('User ' + nickname + ' not found.')
         return redirect(url_for('index'))
 
-    posts = [
-        { 'author': user, 'body': 'Test post #1' },
-        { 'author': user, 'body': 'Text post #2' }
-    ]
+    posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
 
     return render_template( 'user.html',
         user = user,
