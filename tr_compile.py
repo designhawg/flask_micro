f38ed841192c52ba2615ccc2462b43f75064719f
designhawg <designhawg@mac.com>
2014-01-31 21:35:46 -0600

    Added scripts for pybabel

---
+++ tr_compile.py
@@ -0,0 +1,11 @@
+#!/Users/grooves/.virtualenvs/flask_micro/bin/python
+
+import os
+import sys
+
+if sys.platform == 'win32':
+	pybabel = 'flask\\Scripts\\pybabel'
+else:
+	pybabel = '/Users/grooves/.virtualenvs/flask_micro/bin/pybabel'
+
+os.system(py.babel + ' compile -d app/translations')
